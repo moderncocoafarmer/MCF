0492ed1f5a601b1ce99b45a689dd4e69645f25ba
AlanWills <alawills@googlemail.com>
2017-06-26 21:15:30 +0100

    MCF issue #82

---
+++ Assets/Scripts/TransitionToSceneAsync.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionToSceneAsync : MonoBehaviour
+{
+    public string SceneName;
+    public float MinTimeBeforeTransition = 0;
+
+    private float currentTime = 0;
+    private AsyncOperation operation;
+
+    private void Start()
+    {
+        operation = SceneManager.LoadSceneAsync(SceneName);
+        operation.allowSceneActivation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MinTimeBeforeTransition > 0)
+        {
+            currentTime += Time.deltaTime;
+
+            if (currentTime > MinTimeBeforeTransition)
+            {
+                operation.allowSceneActivation = true;
+            }
+        }
+    }
+}
