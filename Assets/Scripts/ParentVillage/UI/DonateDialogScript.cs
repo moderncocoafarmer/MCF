fc8b75d6b57c1a51667140a814acc978f4460b21
Alan Wills <alawills@googlemail.com>
2017-07-10 21:23:09 +0100

    Updated donate dialog code

--- Assets/Scripts/ParentVillage/UI/DonateDialogScript.cs
+++ Assets/Scripts/ParentVillage/UI/DonateDialogScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class DonateDialogScript : MonoBehaviour {
 
     private GameObject donateDialogUI;
+    private GameObject payButton;
     private int amountToPay;
 
 	// Use this for initialization
@@ -13,6 +14,8 @@ public class DonateDialogScript : MonoBehaviour {
     {
         donateDialogUI = transform.FindChild("DonateDialogUI").gameObject;
         donateDialogUI.SetActive(false);
+        payButton = donateDialogUI.transform.FindChild("PayButton").gameObject;
+        payButton.SetActive(false);
     }
 	
 	// Update is called once per frame
@@ -22,14 +25,21 @@ public class DonateDialogScript : MonoBehaviour {
 
     public void SetAmountToPay()
     {
-        amountToPay = donateDialogUI.transform.FindChild("AmountDropdown").GetComponent<Dropdown>().value;
+        string textAmountToPay = donateDialogUI.transform.FindChild("InputField").FindChild("InputFieldText").GetComponent<Text>().text;
+        if (int.TryParse(textAmountToPay, out amountToPay))
+        {
+            payButton.SetActive(amountToPay > 0);
+        }
+        else
+        {
+            payButton.SetActive(false);
+        }
     }
 
     public void CloseAndPay()
     {
         // 770 CFA to 1 pound
-        int actualAmount = amountToPay == 0 ? 1 : 5 * amountToPay;
-        IncomeManager.AddMoney(actualAmount);
+        IncomeManager.AddMoney(amountToPay);
         ChildManager.ApplyEventToAllChildren(new DataPacket(0, 100, 0, 100));
         GameObject.Find("MoneyText").GetComponent<Text>().color = new Color(34.0f / 255, 139.0f / 255, 34.0f / 255, 1.0f);
 
