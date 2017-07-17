5aa40c0b3cd08f7e102fe55f00d23158d7f144ba
Alan Wills <alawills@googlemail.com>
2017-07-17 20:45:17 +0100

    Added all seven children from the start

--- Assets/Scripts/ParentVillage/Children/ChildManager.cs
+++ Assets/Scripts/ParentVillage/Children/ChildManager.cs
@@ -48,6 +48,11 @@ public class ChildManager : MonoBehaviour
         // Add two children to begin with
         GiveBirthToChild();
         GiveBirthToChild();
+        GiveBirthToChild();
+        GiveBirthToChild();
+        GiveBirthToChild();
+        GiveBirthToChild();
+        GiveBirthToChild();
     }
 
     public static void GiveBirthToChild()
